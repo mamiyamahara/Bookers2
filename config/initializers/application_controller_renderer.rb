bced54a6dcf92d9e7cdcdbaec71599231fa28296
vagrant <vagrant@localhost.localdomain>
2020-08-27 03:50:19 +0000

    [Finish] Bookers2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9e4e39b7b18b9c6ef43d88a36a041972dac6e73f5d026bfae28ccd82ec4b21095005b8884d5aa8ae30dc4262abd36e22c0dbeac0eb35c011ada91db56e3c43ab'
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'home/about'
   devise_for :users
 
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
 
 end
